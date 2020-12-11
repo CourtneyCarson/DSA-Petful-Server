046e68b7fa6d6269cd051898ac7388f82a5e205d
Courtney Carson <cmacarson@gmail.com>
2020-12-11 08:29:33 -0800

    queue.js

--- modules/queue/Queue.js
+++ modules/queue/Queue.js
@@ -1,23 +1,71 @@
+class _Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
 class Queue {
   constructor() {
     // Set initial data.
+    this.first = null;
+    this.last = null;
   }
 
   enqueue(data) {
     // Add some data to the queue.
+    const node = new _Node(data);
+
+    if (this.first === null) {
+      this.first = node;
+    }
+    if (this.last) {
+      this.last.next = node;
+    }
+    this.last = node;
   }
 
   dequeue() {
     // Remove some data from the queue.
+    if (this.fist === null) {
+      return;
+    }
+    const node = this.first;
+    this.first = this.first.next;
+
+    if (node === this.last) {
+      this.last = null;
+    }
+    return node.value;
   }
 
   show() {
     // Return the next item in the queue.
+    return this.first.value;
   }
 
   all() {
     // Return all items in the queue.
+      let node = this.first;
+    while (node !== null) {
+      console.log(node.value)
+      node = node.next;
+    }
   }
-}
+    //   let newArray = [];
+    //   let currentNode = queue.first;
 
-module.exports = Queue
+    //   while (currentNode) {
+    //     newArray.push(currentNode.value);
+    //     currentNode = currentNode.next;
+    //   }
+    //   return newArray;
+    // }
+
+
+  
+
+module.exports = Queue;
+
+
+// class 
